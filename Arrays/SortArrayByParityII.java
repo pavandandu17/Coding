4aca57b7674c7084acb192793546ae481897cb39
pavandandu17 <83421837+pavandandu17@users.noreply.github.com>
2022-08-15 21:25:38 +0530

    Create SortArrayByParityII.java

---
+++ Arrays/SortArrayByParityII.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/sort-array-by-parity-ii/
+
+public int[] sortArrayByParityII(int[] nums) 
+{
+        int n = nums.length;
+        
+        int even = 0;
+        int odd = 1;
+        
+        while(even < n && odd < n)
+        {
+            while(even < n && nums[even]%2 == 0)
+                even += 2;
+            while(odd < n && nums[odd]%2 == 1)
+                odd += 2;
+            
+            if(even < n && odd < n)
+            {
+                int temp = nums[even];
+                nums[even] = nums[odd];
+                nums[odd] = temp;
+            }
+        }
+        
+        return nums;
+}
