40f6c8dca5e519d26875193bf4cc5789a418c866
pavandandu17 <83421837+pavandandu17@users.noreply.github.com>
2022-08-16 18:08:42 +0530

    Create Maximum Subarray Sum.java

---
+++ Arrays/Maximum Subarray Sum.java
@@ -0,0 +1,15 @@
+public int maxSubArray(int[] nums) 
+{
+    int ans = Integer.MIN_VALUE;
+
+    int currSum = 0;
+    for(int i=0;i<nums.length;i++)
+    {
+        currSum += nums[i];
+        ans = Math.max(ans, currSum);
+        if(currSum < 0)
+            currSum = 0;
+    }
+
+    return ans;
+}
